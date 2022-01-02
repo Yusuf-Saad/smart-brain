95fa2331766fd797ce7231a46dbf57f37bb4ad49
Yusuf-Saad <yusufmsmsaad@gmail.com>
2022-01-02 13:36:32 -0600

    adding more styles

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,18 +1,24 @@
 import React from 'react';
+import './Navigation.css';
 
 const Navigation = ( {onRouteChange, isSignedIn} ) => {
         if (isSignedIn) {
             return (
-            <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-                <p onClick={() => onRouteChange('signout')}className='f3 link dim black underline pa3 pointer'>Sign Out</p>
-            </nav>
+            <div className='container'>
+                <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+                    <p onClick={() => onRouteChange('signout')} className='f3 link pa3 pointer underline2 p'>Sign Out
+                    </p>
+                </nav>   
+             </div>
           );
         } else {
             return (
+            <div className='contianer'>
                 <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-                    <p onClick={() => onRouteChange('signin')}className='f3 link dim black underline pa3 pointer'>Sign In</p>
-                    <p onClick={() => onRouteChange('register')}className='f3 link dim black underline pa3 pointer'>Register</p>
+                    <p onClick={() => onRouteChange('signin')}className=' f3 link pa3 pointer underline2 p'>Sign In</p>
+                    <p onClick={() => onRouteChange('register')}className='f3 link pa3 pointer underline2 p'>Register</p>
                 </nav>
+            </div>
             );
         }
 }
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Signin.css'
 
 class Signin extends React.Component {  
   constructor(props) {
@@ -39,7 +40,7 @@ class Signin extends React.Component {
   render() {
     const { onRouteChange } = this.props;
   return (
-      <article className="br3 ba black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+      <article className="frosted-glass br3 ba black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
         <main className="pa4 black-80">
         <div className="measure">
           <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
